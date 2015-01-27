93ab780beca84c72a44393f6f1de083d25f33135
Kate Posener <kate.posener@7digital.com>
2015-01-27 15:57:08 +0000

    Final implementation using only mocks

--- TestDoubles.Tests/CustomerRenewalJobTests.cs
+++ TestDoubles.Tests/CustomerRenewalJobTests.cs
@@ -7,23 +7,29 @@ using TestDoubles.System;
 namespace TestDoubles.Tests
 {
 	[TestFixture]
-	public class CustomerRenewalJobTests : ICustomerRepository
+	public class CustomerRenewalJobTests
 	{
-		private DateTime _actualStartDate;
-		private DateTime _actualEndDate;
 		private DateTime _stubbedDateTime = new DateTime(2001, 1, 1);
 		private CustomerRenewalJob _customerRenewalJob;
-		private FakeTimeService _timeService;
 		private IMailSpool _mailSpool;
 		private IEnumerable<Customer> _stubbedCustomers;
+	    private ITimeService _timeService;
+	    private ICustomerRepository _customerRepository;
 
-		[SetUp]
+	    [SetUp]
 		public void SetUp()
 		{
-			_timeService = new FakeTimeService(_stubbedDateTime);
-			_mailSpool = MockRepository.GenerateStub<IMailSpool>();
-			_stubbedCustomers = new List<Customer>{new Customer()};
-			_customerRenewalJob = new CustomerRenewalJob(_timeService, this, _mailSpool);
+		    _timeService = MockRepository.GenerateStub<ITimeService>();
+	        _timeService.Stub(ts => ts.GetDateTime()).Return(_stubbedDateTime);
+
+	        _customerRepository = MockRepository.GenerateStub<ICustomerRepository>();
+	        _stubbedCustomers = new List<Customer>{new Customer()};
+	        _customerRepository.Stub(cr => cr.GetCustomersExpiringBetween(_stubbedDateTime, _stubbedDateTime.AddMonths(1)))
+	                           .Return(_stubbedCustomers);
+
+	        _mailSpool = MockRepository.GenerateStub<IMailSpool>();
+
+	        _customerRenewalJob = new CustomerRenewalJob(_timeService, _customerRepository, _mailSpool);
 
 			_customerRenewalJob.Run();
 		}
@@ -31,14 +37,13 @@ namespace TestDoubles.Tests
 		[Test]
 		public void Should_get_date_and_time()
 		{
-			Assert.IsTrue(_timeService.WasCalled);
+            _timeService.AssertWasCalled(ts => ts.GetDateTime());
 		}
 
 		[Test]
 		public void Should_call_customer_repository_correctly()
 		{
-			Assert.That(_actualStartDate, Is.EqualTo(_stubbedDateTime));
-			Assert.That(_actualEndDate, Is.EqualTo(_stubbedDateTime.AddMonths(1)));
+		    _customerRepository.AssertWasCalled(cr => cr.GetCustomersExpiringBetween(_stubbedDateTime, _stubbedDateTime.AddMonths(1)));
 		}
 
 		[Test]
@@ -46,31 +51,5 @@ namespace TestDoubles.Tests
 		{
 			_mailSpool.AssertWasCalled(ms => ms.SendReminderEmails(_stubbedCustomers));
 		}
-
-		public IEnumerable<Customer> GetCustomersExpiringBetween(DateTime startDate, DateTime endDate)
-		{
-			_actualStartDate = startDate;
-			_actualEndDate = endDate;
-			return _stubbedCustomers;
-		}
 	}
-
-	public class FakeTimeService : ITimeService
-	{
-		private readonly DateTime _stubbedDateTime;
-
-		public FakeTimeService(DateTime dateTime)
-		{
-			_stubbedDateTime = dateTime;
-		}
-
-		public bool WasCalled { get; set; }
-		public DateTime GetDateTime()
-		{
-			WasCalled = true;
-			return _stubbedDateTime;
-		}
-	}
-
-
 }
